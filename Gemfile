d057b4ee852f7cfed5815cd4fcb91d47d97d59fa
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-24 22:53:27 +1000

    adds gems tty-logger tty-cursor arty asciiart. Have some issues with permissions for asciiart and arty

--- Gemfile
+++ Gemfile
@@ -1,5 +1,11 @@
 
+source "https://rubygems.org"
+
 gem 'tty-prompt'
 gem 'colorize'
 gem 'rspec-core', '3.9.2'
 gem 'rspec-expectations', '3.9.2'
+gem 'tty-logger'
+gem 'tty-cursor'
+gem 'arty', '~> 0.0.2'
+gem 'asciiart', '~> 0.0.9'
--- lib/index.rb
+++ lib/index.rb
@@ -1,6 +1,12 @@
+require "tty-prompt"
 
-puts " ____"
-puts "|    |"
-puts "|    |" 
-puts " \u203E\u203E\u203E\u203E"
+@prompt = TTY::Prompt.new
 
+# puts " ____"
+# puts "|    |"
+# puts "|    |" 
+# puts " \u203E\u203E\u203E\u203E" prints overscore
+
+def menu_option
+    user_input = @prompt.select("Play game option: ", ["Single Player", "Two Player", "Load Game", "Quit"])
+end
