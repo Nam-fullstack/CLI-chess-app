8147f169a84c8cb89807581f3c034e20765aaa10
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-25 23:18:23 +1000

    Defines move mechanics for King piece

---
+++ lib/pieces/king.rb
@@ -0,0 +1,11 @@
+
+class Queen
+    def iniatilize(board, usermove)
+
+        @symbol = " \u265A "
+    end
+
+    def move_mechanics
+        [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]]
+    end
+end
