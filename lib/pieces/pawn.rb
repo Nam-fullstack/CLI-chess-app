ff73bb5edb2ec5fa6db60c49a074a2105950f69f
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-25 23:24:05 +1000

    Defines move mechanics for Pawn piece with single and double advances

---
+++ lib/pieces/pawn.rb
@@ -0,0 +1,16 @@
+
+class Pawn
+    def iniatilize(board, usermove)
+
+        @symbol = " \u265F "
+        @moved = fales
+    end
+
+    def single_advance(board)
+        [0, 1]
+    end
+
+    def double_advance(board)
+        [0, 2]
+    end
+end
