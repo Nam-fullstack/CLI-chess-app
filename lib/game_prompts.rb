a1fe78b5f9aa796831cac94b6572a4d6003eae42
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-28 00:06:38 +1000

    Adds some extra game prompts,Start 1-Player game, 2-Player game, Saved game; quit or play again.

--- lib/game_prompts.rb
+++ lib/game_prompts.rb
@@ -21,5 +21,35 @@ module GamePrompts
     end
 
 
-    
+    def game_mode_choices
+        <<~HEREDOC
+
+        \e[45mWelcome to CLI Chess!\e[0m
+
+        Each turn will have two steps:
+
+        \e[45mStep 1:\e[0m
+        Enter the coordinates of the piece that you want to move.
+
+        \e[45mStep 2:\e[0m
+        Enter the coordinates of any legal move \e[102m   \e[0m or capture \e[101m \u265F \e[0m.
+
+        To START, enter one of the following to play:
+
+            \e[96m(1)\e[0m to play a \e[93mNew 1-Player\e[0m game against the computer
+            \e[96m(2)\e[0m to play a \e[93mNew 2-Player\e[0m game
+            \e[96m(3)\e[0m to play a \e[93mSaved\e[0m game
+
+        HEREDOC
+    end
+
+    def repeat_game_choices
+        <<~HEREDOC
+        
+            Would you like to quit game or play again?
+            \e[91m[Q]\e[0m to Quit or \e[96m[P]\e[0m to Play Again
+            
+        HEREDOC
+    end
+
 end
