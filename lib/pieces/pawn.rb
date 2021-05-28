02155c43f98f38c2d80305f53f9eeefa0e6c2b92
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-28 21:31:38 +1000

    Adds en passant mechanics and methods for pawns. Not 100% sure if this is fully functional, but logic should be right. White pawn has to be on 5th rank and 4th for black, previous piece that moved has to be an adjacent pawn that has done a double advance, and final condition is king has to not be in check after en passant capture.

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -67,4 +67,37 @@ class Pawn < Piece
         return [rank, file] if opposing_piece?(rank, file, board.data)
     end
 
+    # defines en passant capture mechanics: 1) has to be adjacent file, only when it is, determine if it's a valid en passant capture
+    def en_passant_capture(board)
+        capture = board.previous_piece&.location
+        return unless capture
+        file_difference = (@location[1] - capture [1]).abs
+        return unless column_difference == 1
+        return capture if valid_en_passant?(board)
+    end
+        
+    # NEED TO CHECK THIS, but think logic should be right, piece that moved previously has to be a pawn that has done a double advance and 
+    # consequently, have en passant as true. Capturing pawn has to be in the correct rank for color
+    # AND also need to check if en passant move and capture will not leave the King in check (legal_en_passant). 
+    def valid_en_passant?(board)
+        en_passant_rank? && 
+            symbol == board.previous_piece.symbol && 
+            board.previous_piece.en_passant && 
+            legal_en_passant_move?(board)
+    end
+
+    # checks if en passant move will leave King in check
+    def legal_en_passant_move?(board)
+        pawn_location = board.previous_piece.location
+        en_passant_move = [pawn_location[0], pawn_location[1] + rank_direction]
+        temp_board = remove_captured_en_passant_pawn(board, pawn_location)
+        legal_capture = remove_ilegal_moves(temp_board, en_passant_move)
+        legal_capture.size.positive?
+    end
+
+    def remove_captured_en_passant_pawn(board, pawn_location)
+        temp_board = Marshal.load(Marshal.dump(board))
+        temp_board.data[pawn_location[0]][pawn_location[1]] = nil
+        temp_board
+    end   
 end
