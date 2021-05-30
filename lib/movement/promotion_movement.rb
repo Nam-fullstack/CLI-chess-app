ae01e953292573bbe924e9d8bb6b80435df4918e
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-30 21:52:54 +1000

    Creates pawn promotion file, and sets up file structure from basic_movement to get access to BasicMovement class

---
+++ lib/movement/promotion_movement.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'basic_movement'
+
+# logic for pawn promotion
+class PawnPromotionMovement < BasicMovement
+    def initialize(board = nil, row = nil, column = nil)
+        super
+    end
+
+
+
+end
