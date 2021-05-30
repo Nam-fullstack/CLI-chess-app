3a38aeecfd4005a6bc4e4194d0b8fc638045b3a4
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-30 21:58:40 +1000

    Adds method for piece removal and new piece selection and creation for pawn promotoion. Need to add prompts for user selection.

--- lib/movement/promotion_movement.rb
+++ lib/movement/promotion_movement.rb
@@ -9,7 +9,28 @@ class PawnPromotionMovement < BasicMovement
     end
 
     def update_pieces(board, coordinates)
-
+        @board = board
+        @row = coordinates[:row]
+        @column = coordinates[:column]
+        update_pawn_promotion_moves
     end
 
+    def update_pawn_promotion_moves
+        remove_capture_piece_observer if @board.data[row][column]
+        remove_pawn_observer
+        remove_original_piece
+        new_piece = new_promotion_piece
+        update_promotion_coordinates(new_piece)
+        update_board_active_piece(new_piece)
+    end
+
+    def new_promotion_piece
+        if @board.mode == :computer && @board.active_piece.color == :black
+            create_promotion_piece('1')
+        else
+            puts pawn_promotion_choices
+            choice = select_promotion_piece
+            create_promotion_piece(choice)
+        end
+    end
 end
