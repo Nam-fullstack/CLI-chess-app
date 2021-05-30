b06e57405efaeb8c06a5a70b9bc81a7aa116d8f0
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-30 16:59:38 +1000

    Adds castling movement class and logic for pieces moving, being removed, and new location after castling King side or castle Queen side. Also uses King's location to determine which rook to move. Haven't tested this yet, but hopefuly it all works, thought about the logic for quite a long time now.

--- lib/movement/castling_movement.rb
+++ lib/movement/castling_movement.rb
@@ -1,7 +1,60 @@
+# frozen_string_literal: true
 
+require_relative 'basic_movement'
 
-# not 100% sure how to do this
+# logic for castling movement
+class CastlingMovement < BasicMovement
+    def initialize(board = nil, row = nil, column = nil)
+        super
+    end
 
-def castling
-    puts "I'll figure this out"
+    def update_pieces(board, coordinates)
+        @board = board
+        @row = coordinates[:row]
+        @column = coordinates[:column]
+        update_castling_moves
+    end
+
+    # main logic for castling
+    def update_castling_moves
+        update_new_coordinates
+        remove_original_piece
+        update_active_piece_location
+        castling_rook = find_castling_rook
+        remove_original_rook_piece
+        update_castling_coordinates(castling_rook)
+        update_castling_piece_location(castling_rook)
+    end
+
+    def find_castling_rook
+        @board.data[row][old_rook_file]
+    end
+
+    def remove_original_rook_piece
+        @board.data[row][old_rook_file] = nil
+    end
+
+    def update_castling_coordinates(rook)
+        @board.data[row][new_rook_file] = rook
+    end
+
+    def update_castling_piece_location(rook)
+        rook.update_location(row, new_rook_file)
+    end
+
+    private
+
+    # determines the original rook location based on the file (column) of the King's move
+    def old_rook_file
+        king_side = 7
+        queen_side = 0
+        column == 6 ? king_side : queen_side
+    end
+
+    # determines the rook's new location based on the file (column) of the King's move
+    def new_rook_file
+        king_side = 5
+        queen_side = 3
+        column == 6 ? king_side : queen_side
+    end
 end
