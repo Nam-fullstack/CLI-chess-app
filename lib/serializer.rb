101b928bb846079b4b4bf864c212a6694816a7a4
Nam-fullstack <namnguyen13_@hotmail.com>
2021-05-31 21:29:06 +1000

    Creates serializer.rb file to save and load chess games. Just created skeleton of empty methods i.e. def save_game    def load_game etc.

---
+++ lib/serializer.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Contains methods to save and/or load a game
+module Serializer
+    def save_game
+    
+    end
+
+    def create_filename
+
+    end
+
+    def load_game
+
+    end
+
+    def find_saved_file
+
+    end
+
+    def print_saved_games
+
+    end
+
+    def select_saved_game
+
+    end
+
+    
+
+
+
+
+
+
+end
