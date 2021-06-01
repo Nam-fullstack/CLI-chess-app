53aa109e9f22df7bf78be044044a34acff9bb207
Nam-fullstack <namnguyen13_@hotmail.com>
2021-06-01 15:34:35 +1000

    Corrects formatting for game_prompts.rb

--- lib/game_prompts.rb
+++ lib/game_prompts.rb
@@ -2,61 +2,54 @@
 
 require 'colorize'
 
+# contains text prompts for chess game
 module GamePrompts
-    # def menu_option
-    #     @prompt = TTY::Prompt.new
-    #     @user_input = @prompt.select("Play game option: ", ["\e[1mSingle Player\e[0m", "\e[1mTwo Player\e[0m", "\e[1mLoad Game\e[0m", "\e[1mHow to Play\e[0m", "\e[1mQuit\e[0m"])
-    #     puts "This is the users selection: #{@user_input}"
-    # end
-
     def select_game_mode
         prompt = TTY::Prompt.new
-        menu_select = prompt.select("MENU", 
-                                    ["1 - Single Player", 
-                                    "2 - Two Player", 
-                                    "3 - Load Game", 
-                                    "4 - How to Play", 
+        menu_select = prompt.select("MENU",
+                                    ["1 - Single Player",
+                                    "2 - Two Player",
+                                    "3 - Load Game",
+                                    "4 - How to Play",
                                     "5 - Quit"])
         @mode = menu_select.to_i
     end
 
     def return_to_menu
         @back_to_menu = TTY::Prompt.new
-        @navigation = @back_to_menu.select(" ", ["Back to Menu", "Start a New Game", "Exit"])
+        @navigation = @back_to_menu.select(' ', ['Back to Menu', 'Start a New Game', 'Exit'])
         puts "This is the users selection: #{@navigation}"
     end
-
 
     # def select_game_mode
     #     user_mode_select = gets.strip
     #     return user_mode_select if user_mode_select.match?(/^[12345]$/)   # will only return if input matches defined numbers in []
-        
+
     #     puts "Input error! Please select from one of the menu options: 1, 2, 3, 4 or 5."
     #     select_game_mode
     # end
-    
+
     def repeat_game
         puts repeat_game_choices
         input = gets.strip
         choice = input.upcase == 'Q' ? exit_program : :repeat
         return choice if input.match?(/^[QP]$/i)
 
-        puts "Input error! Please enter Q or P."
+        puts 'Input error! Please enter Q or P.'
         repeat_game
     end
 
     def final_message
         return unless @player_count.positive?
+
         if @board.king_in_check?(@current_turn)
             puts "\e[96m CHECKMATE!! #{previous_color.upcase}\e[0m WINS!"
-        else 
+        else
             puts "\n\e[96m DRAW! STALEMATE! \n\n"
         end
     end
 
     private
-
-
 
     # def game_mode_choices2
     #     <<~HEREDOC
@@ -84,21 +77,21 @@ module GamePrompts
         system 'clear'
         puts "HOW TO PLAY \n\n".colorize(:yellow)
         puts "Player's turn will comprise of 2 steps. \n".colorize(:cyan)
-        puts "STEP 1: ".colorize(:magenta) + "Select the coordinates of piece you wish to move." + " eg. d2 \n".colorize(:cyan)
-        puts "STEP 2: ".colorize(:magenta) + "Enter coordinates of valid move: square(s) highlighted \e[102m   \e[0m or capture \e[101m \u265F \e[0m\n\n"
-        
+        puts 'STEP 1: '.colorize(:magenta) + 'Select the coordinates of piece you wish to move.' + " eg. d2 \n".colorize(:cyan)
+        puts 'STEP 2: '.colorize(:magenta) + "Enter coordinates of valid move: square(s) highlighted \e[102m   \e[0m or capture \e[101m \u265F \e[0m\n\n"
+
         # "To go back to the Main Menu, press \e[4mM\e[0m"
 
         sleep(5)
-        
+
     end
 
     def repeat_game_choices
         <<~HEREDOC
-        
+
             Would you like to start a New Game or Quit Game?
             \e[96m[P]\e[0m to Play A New Game or \e[91m[Q] \e[0m to Quit
-            
+
         HEREDOC
     end
 
@@ -123,15 +116,18 @@ module GamePrompts
     def user_move_selection
         <<~HEREDOC
 
-        Please enter coordinates for a legal move, square(s) highlighted \e[102m   \e[0m or capture \e[101m \u265F \e[0m. 
+        Please enter coordinates for a legal move:
+        square(s) highlighted \e[102m   \e[0m or capture \e[101m \u265F \e[0m. 
 
         HEREDOC
     end
 
     def en_passant_warning
         puts "\e[96mPossibility to capture the opposing pawn that just moved.\e[0m" 
-        puts "\e[91mTo capture this pawn en passant\e[0m (in passing),\nplease enter the \e[41mhighlighted coordinates\e[0m."
-        puts "As part of en passant, your\e[96m pawn will be moved \nto the square in front of the captured pawn\e[0m."
+        puts "\e[91mTo capture this pawn en passant\e[0m (in passing),"
+        puts "please enter the \e[41mhighlighted coordinates\e[0m."
+        puts "As part of en passant, your\e[96m pawn will be moved"
+        puts "to the square in front of the captured pawn\e[0m."
     end
 
     def king_check_warning
@@ -154,9 +150,9 @@ module GamePrompts
 
     def exit_program
         puts "Are you sure you want to Exit?    'yes' to quit"
-        input == "yes" ? quit_app : return_to_menu
+        input == 'yes' ? quit_app : return_to_menu
     end
-    
+
     def quit_app
         system 'clear'
         puts "Thank you for playing CLI Chess! \nHope you enjoyed this game!"
