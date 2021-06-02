018e79bbf559897f8ce30bf408e9c63405b62a85
Nam-fullstack <namnguyen13_@hotmail.com>
2021-06-02 16:00:43 +1000

    Adds commentary for yaml configuration file for rubocop autoformatter gem.

--- lib/landing_banner.rb
+++ lib/landing_banner.rb
@@ -1,6 +1,6 @@
 def display_ansi
     begin
-    File.foreach("./banner/3-350.txt") { |line| puts line }
+    File.foreach("./banner/6-350.txt") { |line| puts line }
     rescue
         puts "Unable to find file"
     end
